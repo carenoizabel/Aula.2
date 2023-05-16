0dee8a0bbfa2e2a05ad71a519f1ddd62bc8db05a
iza careno <131816960+carenoizabel@users.noreply.github.com>
2023-05-16 12:29:35 -0400

    personalizei minha página com css, logo depois arrumei minha página do js pois estava deixando a imagem grande

--- script.js
+++ script.js
@@ -26,8 +26,7 @@ class ProdutoDestaque extends Produto{
         <div>${this.datadeCadastro}</div>
         <p>${this.descricao}</p>
         <h3>${this.preco}</h3>
-        <div>${this.imgDestaque}</div>
-        <img src="">
+        <img id="imagem-destaque" src="${this.imgDestaque}">
         `
     }
 }
@@ -37,3 +36,7 @@ console.log(produtoDestaque.mostrarProdutoDestaque())
 
 const produtosDestaque = document.getElementById("produto-destaque");
 produtosDestaque.insertAdjacentHTML('afterbegin',   produtoDestaque.mostrarProdutoDestaque());
+
+const imagemDestaqueElement = document.getElementById("imagem-destaque");
+imagemDestaqueElement.style.maxWidth = "100%";
+imagemDestaqueElement.style.height = "auto";
