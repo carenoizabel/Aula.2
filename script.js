1d40ddddccd51529993c7d1988f42eb2409bed77
iza careno <131816960+carenoizabel@users.noreply.github.com>
2023-05-23 00:41:53 -0400

    organizei meu site, adicionei 6 produtos e uma imagem destaque, estilizei com css, usei o método que aprendi em alg 2, pois usando o outro método não consegui fazer, meu site é sobre quadros de artes

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
-// divListaProduto.insertAdjacentHTML('afterbegin', '');
-
-class Produto{
-    constructor(nome, datadeCadastro, descricao, preco){
-        this.nome = nome;
-        this.datadeCadastro = datadeCadastro;
-        this.descricao = descricao;
-        this.preco = preco;
+class Produto {
+    constructor(nome, artista, descricao, preco, imagem) {
+      this.nome = nome;
+      this.artista = artista;
+      this.descricao = descricao;
+      this.preco = preco;
+      this.imagem = imagem;
     }
-    mostrarProduto(){
-        return this.nome + ' ' + this.datadeCadastro + ' ' + this.descricao + ' ' + this.preco
+  
+    mostrarProduto() {
+      return this.nome + ' ' + this.artista + ' ' + this.descricao + ' ' + this.preco;
     }
-}
-
-const produto = new Produto("IFCorporation LTDA", "2023-04-19", "IFMS - Campus Naviraí", "999.99")
-console.log(produto.mostrarProduto())
-
-class ProdutoDestaque extends Produto{
-    constructor(nome, datadeCadastro, descricao, preco, imgDestaque){
-        super(nome, datadeCadastro, descricao, preco);
-        this.imgDestaque = imgDestaque;
+  }
+  
+  class ProdutoDestaque extends Produto {
+    constructor(nome, artista, descricao, preco, imagemDestaque) {
+      super(nome, artista, descricao, preco);
+      this.imagemDestaque = imagemDestaque;
     }
-    mostrarProdutoDestaque(){
-        return  `
+  
+    mostrarProdutoDestaque() {
+      return `
         <h1>${this.nome}</h1>
-        <div>${this.datadeCadastro}</div>
+        <div>${this.artista}</div>
         <p>${this.descricao}</p>
         <h3>${this.preco}</h3>
-        <img id="imagem-destaque" src="${this.imgDestaque}">
-        `
+        <img id="imagem-destaque" src="${this.imagemDestaque}">
+      `;
     }
-}
-
-const produtoDestaque = new ProdutoDestaque("IFCorporation", "2023-04-19", "IFMS - Campus Nova Andradina", "999.99", "logo_ifms.png");
-console.log(produtoDestaque.mostrarProdutoDestaque())
-
-const produtosDestaque = document.getElementById("produto-destaque");
-produtosDestaque.insertAdjacentHTML('afterbegin',   produtoDestaque.mostrarProdutoDestaque());
-
-const imagemDestaqueElement = document.getElementById("imagem-destaque");
-imagemDestaqueElement.style.maxWidth = "100%";
-imagemDestaqueElement.style.height = "auto";
+  }
+  
+  const produtoDestaque = new ProdutoDestaque("BEAUTIFUL ANGEL", "dada22", "Esta obra retrata uma figura angelical com traços delicados e expressão serena, envolta em cores suaves e harmoniosas. A artista criou uma atmosfera de beleza e tranquilidade, transmitindo uma sensação de paz e serenidade ao observador.", "US$ 520", "https://urbanarts.vteximg.com.br/arquivos/ids/2629379/124173.jpg?v=637184086729730000");
+  
+  const produtos = [
+    new Produto("BLOOM 7", "dada22", "Essa peça exala uma sensação de florescimento e renovação, com suas cores vivas e composição dinâmica.", "US$ 199", "https://urbanarts.vteximg.com.br/arquivos/ids/3523946/218075-PM-129-11.jpg?v=637185749993430000"),
+    new Produto("BOSSA IN JAZZ", "Ana Paula Hoppe", "Ela traz consigo uma sensação de sofisticação e relaxamento, com cores vibrantes.", "US$ 199", "https://urbanarts.vteximg.com.br/arquivos/ids/3408699/179470-PM-133-63.jpg?v=637185046844200000"),
+    new Produto("THIS CITY REFIX", "Ruben Ireland", "Essa peça apresenta uma representação visual impactante e intrigante de uma menina jovem.", "US$ 199", "https://urbanarts.vteximg.com.br/arquivos/ids/3510346/184884-PM-129-11.jpg?v=637185127276830000"),
+    new Produto("TROPICAL GIRL 23", "ThingDesign", "Com sua estética tropical e colorida, ele traz uma atmosfera alegre e descontraída para qualquer ambiente.", "US$ 199", "https://urbanarts.vteximg.com.br/arquivos/ids/8601757/450010_Ampliada.jpg?v=637552552358800000"),
+    new Produto("REALEZA AFRICANA", "Fredissimo", "Sua pele é representada em tons ricos e profundos, simbolizando a diversidade e a beleza da pele negra.", "US$ 199", "https://urbanarts.vteximg.com.br/arquivos/ids/10041713/156188_Ampliada.jpg?v=637691305843500000"),
+    new Produto("CENTELHA 3 DA HELO", "Helo Moraes", "A mulher é retratada em um momento de contemplação, transmitindo uma aura de mistério e serenidade.", "US$ 199", "https://urbanarts.vteximg.com.br/arquivos/ids/8893924/865632_Ampliada.jpg?v=637562913364800000"),
+  ];
+  
+  const produtoDestaqueElement = document.getElementById("produto-destaque");
+  produtoDestaqueElement.innerHTML = produtoDestaque.mostrarProdutoDestaque();
+  
+  const listaProdutosElement = document.getElementById("lista-produtos");
+  produtos.forEach(produto => {
+    const produtoElement = document.createElement("div");
+    produtoElement.classList.add("produto");
+    produtoElement.innerHTML = `
+      <img src="${produto.imagem}" alt="${produto.nome}">
+      <h3>${produto.nome}</h3>
+      <p>${produto.descricao}</p>
+      <p>${produto.preco}</p>
+      <p>${produto.artista}</p>
+    `;
+    listaProdutosElement.appendChild(produtoElement);
+  });
+  
